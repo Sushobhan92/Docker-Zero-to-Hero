066fd4d3ecd4751ce12d694b96ebf441efe7636c
Sushobhan Dutta <sushobhan.dutta14@gmail.com>
2025-12-05 15:32:11 +0530

    Update app.py

--- examples/first-docker-file/app.py
+++ examples/first-docker-file/app.py
@@ -1,1 +1,12 @@
 print("Hello World")
+
+print("Printing numbers from 1 to 5:\n")
+for i in range(1, 6):
+    print(f"Number: {i}")
+
+def greet(name):
+    return f"Hello, {name}!"
+
+print(greet("Docker"))
+
+
